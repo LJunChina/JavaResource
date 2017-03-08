211ee4a9a0084c6153cd16ad59f7f69159197351
unknown <>
2017-03-08 21:38:28 +0800

    dieLock

---
+++ DieLock/src/com/jon/dielock/MyLock.java
@@ -0,0 +1,7 @@
+package com.jon.dielock;
+
+public class MyLock{
+	//ÑìΩ®É…∞—Êiå¶œÛ
+	public static final Object objA = new Object();
+	public static final Object objB = new Object();
+}
---
+++ DieLock/src/com/jon/dielock/TestDieLock.java
@@ -0,0 +1,13 @@
+package com.jon.dielock;
+
+public class TestDieLock {
+
+	public static void main(String[] args) {
+		DieLock dl1 = new DieLock(true);
+		DieLock dl2 = new DieLock(false);
+		
+		dl1.start();
+		dl2.start();
+	}
+
+}
