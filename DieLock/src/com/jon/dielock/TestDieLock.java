94d1be827e45657983df1d21ceaf81fcf5c081a2
Jon_China <503665965@qq.com>
2017-05-11 22:19:17 +0800

    Update TestDieLock.java

--- DieLock/src/com/jon/dielock/TestDieLock.java
+++ DieLock/src/com/jon/dielock/TestDieLock.java
@@ -5,7 +5,7 @@ public class TestDieLock {
 	public static void main(String[] args) {
 		DieLock dl1 = new DieLock(true);
 		DieLock dl2 = new DieLock(false);
-		
+		//分别启动两个线程
 		dl1.start();
 		dl2.start();
 	}
